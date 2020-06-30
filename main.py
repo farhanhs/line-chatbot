c77c46a5102c4316e0eed4144f7152399fd64c4e
sean0020002 <sean002002@gmail.com>
2020-07-01 00:56:45 +0800

    fix requirement

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, abort
+#flask網頁工具
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('i8QhiXQup8rdl6IPnGgIMp65u8Z54IjMrBWkvUMf5wCElTrDd8Qe/8vgf5AksmRI5ZaxAptKhyAsZwCGocAnRA78MXndZ0IS5MxAwCwoIUYsGSd3Jt8ifVEvI8iIGhOv+52sgs+Ya4eqHunaJESP6AdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('b1ec8ca0fbdca57105c818c8f0d85940')
+
+#準備一個路徑URN
+#此路徑為simulate-ai
+#用戶訪問的時候，回傳1+1
+
+
+@app.route("/fake")
+def fake():
+    
+    crewresult = request.get_data('https:// www.toutiao.com')
+    
+    return crewresult.txt
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
