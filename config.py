933b1d417fd5d739b9d260c5648d0bec317e33a0
sean0020002 <sean002002@gmail.com>
2020-07-21 19:20:25 +0800

    imgur+postgre

---
+++ config.py
@@ -0,0 +1,10 @@
+# imgur key
+client_id = 'e19a3d9b71fe2b5'
+client_secret = '1e67a47d895991986360965e91c3dfa363e16672'
+album_id = 'a/ZzwUqdd'
+access_token = '5ef22e04016db4ffa4de36d0ccf942a3710241b7'
+refresh_token = 'ad4ba838e0e43765a1a2d67e66a7b1c367dc0e18'
+
+# line bot key
+line_channel_access_token = 'i8QhiXQup8rdl6IPnGgIMp65u8Z54IjMrBWkvUMf5wCElTrDd8Qe/8vgf5AksmRI5ZaxAptKhyAsZwCGocAnRA78MXndZ0IS5MxAwCwoIUYsGSd3Jt8ifVEvI8iIGhOv+52sgs+Ya4eqHunaJESP6AdB04t89/1O/w1cDnyilFU='
+line_channel_secret = 'b1ec8ca0fbdca57105c818c8f0d85940'
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, request, abort
 #flask網頁工具
 from linebot import (
@@ -10,14 +11,10 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, FollowEvent, TemplateSendMessage, ButtonsTemplate, PostbackAction, ImageMessage, MessageAction, URIAction,
 )
 import os
-#import connect_sql as cs
+import connect_sql as cs
+from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, \
+    line_channel_secret
 
-import boto3
-s3Client = boto3.client(
-    's3',
-    aws_access_key_id='AKIAR4NDUH53GWDLQFNM',
-    aws_secret_access_key='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'
-)
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('i8QhiXQup8rdl6IPnGgIMp65u8Z54IjMrBWkvUMf5wCElTrDd8Qe/8vgf5AksmRI5ZaxAptKhyAsZwCGocAnRA78MXndZ0IS5MxAwCwoIUYsGSd3Jt8ifVEvI8iIGhOv+52sgs+Ya4eqHunaJESP6AdB04t89/1O/w1cDnyilFU=')
@@ -46,47 +43,9 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    #responce message
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-    user_id = event.source.user_id
-    profile = line_bot_api.get_profile(user_id)
-
-    print(profile.display_name)
-    print(profile.user_id)
-    print(profile.picture_url)
-    print(profile.status_message)
-    package_id = "1"
-    sticker_id = "2"
-    # package_id = "1181660"
-    # sticker_id = "7389429"
-    try:
-        line_bot_api.push_message(user_id, TextSendMessage(text=profile.display_name + ',你好。請問需要什麼幫助？'))
-        line_bot_api.push_message(user_id, TextSendMessage(text='我是印度大蟒蛇'))
-        line_bot_api.push_message(user_id, StickerSendMessage(package_id=package_id, sticker_id=sticker_id))
-    except LineBotApiError as e:
-    # error handle
-        print(e.status_code)
-        print(e.request_id)
-        print(e.error.message)
-        print(e.error.details)
-        raise e
-
-
 @handler.add(FollowEvent)
 def handle_follow(event):
-    #user_profile = line_bot_api.get_profile(event.source.user_id)
     user_id = event.source.user_id
-    #print(user_profile.user_id)
-    #with open('./user.txt', 'a') as myfile:
-    #    myfile.write(json.dumps(vars(user_profile)))
-    #生成範本消息
-    #alt_text:消息在賴聊天列表的替代文字
-    #title
-    #action
     #   messageaction
     #      label:案件的字樣
     #      text:
@@ -98,45 +57,108 @@ def handle_follow(event):
     #       text：發一個textevent
     #       data:會發一個postbackenevt給server
     buttons_template_message = TemplateSendMessage(
-        alt_text='打你有什麼錯',
+        alt_text='為什麼你可以賣這麼便宜！',
         template=ButtonsTemplate(
             thumbnail_image_url='https://i2.kknews.cc/SIG=k2b84c/n61o000109s2r818333p.jpg',
-            title='課程資訊',
-            text='選擇你要的課程資訊',
+            title='競拍表單',
+            text='請選擇服務類型',
             actions=[
-                PostbackAction(
-                    label='postback',
-                    display_text='postback text',
-                    data='action=buy&itemid=1'
+                MessageAction(
+                    label='今日拍賣',
+                    text='我要看商品',
                 ),
                 MessageAction(
-                    label='message',
-                    text='message text'
+                    label='我要拍賣',
+                    text='我要拍賣商品',
                 ),
-                URIAction(
-                    label='uri',
-                    uri='http://example.com/'
-                )
+                MessageAction(
+                    label='抽個大吉',
+                    text='postback text',
+                ),
             ]
         )
     )
     line_bot_api.push_message(user_id,buttons_template_message)
 
-#收到圖片消息，從line上面抓回來並且以檔名消息id命名，上傳到s3,位置student01/id.jpg
-@handler.add(MessageEvent, message=ImageMessage)
-def handle_image_message(event):
-    #message_id = event.source.user_id
-    #請line抓圖片
-    image_temp_variable = line_bot_api.get_message_content(event.message.id)
-    #圖片儲存
-    imgname = event.message.id+'.jpg'
-    with open (imgname,'wb')as f:
-        for chunk in image_temp_variable.iter_content():
-            f.write(chunk)
-    #upload s3
-    s3Client.upload_file(imgname, 'iii-tutorial-v2', 'student01/'+imgname)
-    image_id_text_send_messages = TextSendMessage(event.message.id)
-    line_bot_api.reply_message(event.reply_token, image_id_text_send_messages)
+@handler.add(MessageEvent, message=(ImageMessage, TextMessage))
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+    user_id = event.source.user_id
+    profile = line_bot_api.get_profile(user_id)
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+            tempfile_path = tf.name
+
+        dist_path = tempfile_path + '.' + ext
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+        try:
+            client = ImgurClient(client_id, client_secret,
+                                 access_token, refresh_token)
+            config = {
+                'album': album_id,
+                'name': 'Catastrophe!',
+                'title': 'Catastrophe!',
+                'description': 'Cute kitten being cute on '
+            }
+            path = os.path.join('static', 'tmp', dist_name)
+            client.upload_from_path(path, config=config, anon=False)
+            os.remove(path)
+            print(path)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳成功'))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳失敗'))
+        return 0
+
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    elif isinstance(event.message, TextMessage):
+        if event.message.text =='我要看商品':
+            try:
+                client = ImgurClient(client_id, client_secret)
+                images = client.get_album_images(album_id)
+                index = random.randint(0, len(images) - 1)
+                url = images[index].link
+                image_message = ImageSendMessage(
+                    original_content_url=url,
+                    preview_image_url=url
+                    )
+                line_bot_api.push_message(user_id, TextSendMessage(
+                    text=profile.display_name + ',你好。現在競拍的商品為：'))
+                line_bot_api.reply_message(
+                    event.reply_token, image_message)
+                line_bot_api.push_message(user_id, TextSendMessage(text='底標為：'))
+                line_bot_api.push_message(user_id, StickerSendMessage(
+                        package_id=package_id, sticker_id=sticker_id))
+            except LineBotApiError as e:
+                    # error handle
+                    print(e.status_code)
+                    print(e.request_id)
+                    print(e.error.message)
+                    print(e.error.details)
+                    raise e
+        if event.message.text == "看看大家都傳了什麼圖片":
+
+            return 0
+        else:
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text=' yoyo'),
+                    TextSendMessage(text='請傳一張圖片給我')
+                ])
+            return 0
 
 if __name__ == "__main__":
     app.run()
